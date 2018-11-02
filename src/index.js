ad60f51df63fe727ff3f1283d204921475b153a6
Punnadit Trussat <punnadittr@gmail.com>
2018-11-02 14:57:35 +0700

    Add robot card and cardlist components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CardList from './CardList';
+import { robots } from './robots';
 import './index.css';
-import App from './App';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
